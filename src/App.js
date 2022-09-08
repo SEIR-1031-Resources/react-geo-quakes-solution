f86b7044718e84dcbad7482588a82ce2d42561b0
maker-jws <ws.joshua@gmail.com>
2022-09-07 18:09:47 -0600

    updated resources

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="mapContainer">
+        ...put Map Component here...
+      </div>
+      <div className="quakeContainer">
+        <h1>Earthquakes from the past week:</h1>
+        ...put Quakes Component here...
+      </div>
+    </div>
+  );
+}
+
+export default App;
